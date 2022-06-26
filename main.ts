bb06160c20eb8c7571fe145e672938f76b87d38e
Daiki48 <99887018+NakashimaDaiki@users.noreply.github.com>
2022-06-26 13:48:23 +0900

    main.ts update

--- main.ts
+++ main.ts
@@ -14,6 +14,6 @@ export async function main(denops: Denops): Promise<void>{
 
   const n = denops.name;
   await denops.cmd(
-    `command! DenopsHello call denops#notify("${n}", "world", [denops#request("${n}", "hello",[])])`,
+    `command! DenopsDaiki call denops#notify("${n}", "world", [denops#request("${n}", "hello",[])])`,
   );
 }
