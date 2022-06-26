8e16558ddb0a1aa2506b61cb3959a4aa30e38b33
Daiki48 <99887018+NakashimaDaiki@users.noreply.github.com>
2022-06-26 17:09:02 +0900

    update

---
+++ denops/hellodaiki/main.ts
@@ -0,0 +1,18 @@
+import { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts";
+
+export async function main(denops: Denops): Promise<void> {
+  denops.dispatcher = {
+    hello() {
+      return Promise.resolve("daiki");
+    },
+    async world(text: unknown) {
+      await denops.cmd("echomsg text", {
+        text,
+      });
+    },
+  };
+  const n = denops.name;
+  await denops.cmd(
+    `command! DenopsHello call denops#notify("${n}", "world", [denops#request("${n}", "hello", [])])`
+  );
+}
